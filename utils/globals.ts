0772ad75f99df4d660fa1af05c2a08aa7ae3d11c
miguelhuayhua <miguelhuayhuac2@gmail.com>
2024-09-11 16:18:55 -0400

    version file

--- utils/globals.ts
+++ utils/globals.ts
@@ -1,3 +1,3 @@
 const domain = 'http://localhost:3000';
-const fileDomain = 'http://localhost:4000';
+const fileDomain = 'http://ip172-18-0-21-crgv8mqim2rg00aaad40-4000.direct.labs.play-with-docker.com';
 export { domain, fileDomain };
