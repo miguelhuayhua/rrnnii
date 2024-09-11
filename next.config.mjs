861e881ded9a5aa14c2401fef04bfaf390c04233
miguelhuayhua <miguelhuayhuac2@gmail.com>
2024-09-11 16:38:15 -0400

    version play

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
         remotePatterns: [
             {
                 hostname: 'localhost'
+            },
+            {
+                hostname:'play-with-docker'
             }
         ]
     }
