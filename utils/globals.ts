75a89e2a66f2d449edde9a08c08687572b7e83da
miguelhuayhua <miguelhuayhuac2@gmail.com>
2024-10-03 16:28:22 -0400

    version imagen

--- utils/globals.ts
+++ utils/globals.ts
@@ -1,3 +1,3 @@
 const domain = 'http://localhost:3000';
-const fileDomain = 'http://ip172-18-0-60-crvc36gl2o9000bqvcm0-4000.direct.labs.play-with-docker.com';
+const fileDomain = 'http://ip172-18-0-48-crvfs32im2rg00b3967g-4000.direct.labs.play-with-docker.com';
 export { domain, fileDomain };
