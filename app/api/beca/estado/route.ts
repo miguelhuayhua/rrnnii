cd7d583a237e0867cb17cf45c25e1dc25bbfe1a3
miguelhuayhua <miguelhuayhuac2@gmail.com>
2024-10-17 16:41:24 -0400

    version 1

---
+++ app/api/beca/estado/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest } from "next/server";
+import { prisma } from "../../client";
+const POST = async (request: NextRequest) => {
+    let { estado, id } = await request.json();
+    try {
+        await prisma.beca.update({
+            data: { estado }, where: { id }
+        });
+        return Response.json({ error: false, mensaje: `Beca ${estado ? 'Activada' : 'Desactivada'}` });
+    } catch (error) {
+        console.log(error)
+        return Response.json({
+            error: true,
+            mensaje: 'Error al modificar el estado'
+        });
+    }
+}
+
+export { POST };
